06c1342c6feef0434931197ef1638399cd979e9e
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2022-05-31 19:12:19 -0300

    seting lsp servers and returning on an array

---
+++ nvim/lua/Plugins/servers.lua
@@ -0,0 +1,368 @@
+servers = {
+    -- Server list, uncomment the ones you need to download automatically
+    
+        -- AWK 	
+    -- 'awk_ls',
+
+        -- Angular 	
+    -- 'angularls',
+
+        -- Ansible 	
+    -- 'ansiblels',
+
+        -- Arduino 
+    -- 'arduino_language_server',
+
+        -- Assembly 
+    -- 'asm_lsp',
+
+        -- Astro 	
+    -- 'astro',
+
+        -- Bash 	
+    -- 'bashls',
+
+        -- Beancount 	
+    -- 'beancount',
+
+        -- Bicep 	
+    -- 'bicep',
+
+        -- C 	
+    -- 'ccls',
+
+        -- C 	
+    -- 'clangd',
+
+        -- C
+    -- 'csharp_ls',
+
+        -- C
+    -- 'omnisharp',
+
+        -- C
+    -- 'ccls',
+
+        -- C
+    -- 'clangd',
+
+        -- CMake 	
+    -- 'cmake',
+
+        -- CSS 	
+    -- 'cssls',
+
+        -- CSS 	
+    -- 'cssmodules_ls',
+
+        -- Clarity 	
+    -- 'clarity_lsp',
+
+        -- Clojure 	
+    -- 'clojure_lsp',
+
+        -- CodeQL 	
+    -- 'codeqlls',
+
+        -- Crystal 	
+    -- 'crystalline',
+
+        -- Crystal 	
+    -- 'scry',
+
+        -- Cucumber 	
+    -- 'cucumber_language_server',
+
+        -- Dart 	
+    -- 'dartls',
+
+        -- Deno 	
+    -- 'denols',
+
+        -- Dhall 	
+    -- 'dhall_lsp_server',
+
+        -- Diagnostic 
+    -- 'diagnosticls',
+
+        -- Dlang 	
+    -- 'serve_d',
+
+        -- Docker 	
+    -- 'dockerls',
+
+        -- Dot 	
+    -- 'dotls',
+
+        -- EFM 
+    -- 'efm',
+
+        -- ESLint 
+    -- 'eslint',
+
+        -- Elixir 	
+    -- 'elixirls',
+
+        -- Elm 	
+    -- 'elmls',
+
+        -- Ember 	
+    -- 'ember',
+
+        -- Emmet 	
+    -- 'emmet_ls',
+
+        -- Erlang 	
+    -- 'erlangls',
+
+        -- F
+    -- 'fsautocomplete',
+
+        -- Flux 	
+    -- 'flux_lsp',
+
+        -- Foam 
+    -- 'foam_ls',
+
+        -- Fortran 	
+    -- 'fortls',
+
+        -- Go 	
+    -- 'golangci_lint_ls',
+
+        -- Go 	
+    -- 'gopls',
+
+        -- Grammarly 	
+    -- 'grammarly',
+
+        -- GraphQL 	
+    -- 'graphql',
+
+        -- Groovy 	
+    -- 'groovyls',
+
+        -- HTML 	
+    -- 'html',
+
+        -- Haskell 	
+    -- 'hls',
+
+        -- Haxe 	
+    -- 'haxe_language_server',
+
+        -- Hoon 	
+    -- 'hoon_ls',
+
+        -- JSON 	
+    -- 'jsonls',
+
+        -- Java 	
+    -- 'jdtls',
+
+        -- JavaScript 	
+    -- 'quick_lint_js',
+
+        -- JavaScript 	
+    -- 'tsserver',
+
+        -- Jsonnet 	
+    -- 'jsonnet_ls',
+
+        -- Julia 	
+    -- 'julials',
+
+        -- Kotlin 	
+    -- 'kotlin_language_server',
+
+        -- LaTeX 	
+    -- 'ltex',
+
+        -- LaTeX 	
+    -- 'texlab',
+
+        -- Lelwel 	
+    -- 'lelwel_ls',
+
+        -- Lua 	
+    -- 'sumneko_lua',
+
+        -- Markdown 	
+    -- 'prosemd_lsp',
+
+        -- Markdown 	
+    -- 'remark_ls',
+
+        -- Markdown 	
+    -- 'zk',
+
+        -- Metamath Zero 	 
+    -- 'mm0_ls',
+
+        -- Nickel 	
+    -- 'nickel_ls',
+
+        -- Nim 	
+    -- 'nimls',
+
+        -- OCaml 	
+    -- 'ocamlls',
+
+        -- OCaml 	
+    -- 'ocamllsp',
+
+        -- Objective C
+    -- 'ccls',
+
+        -- OneScript 1C:Enterprise  
+    -- 'bsl_ls',
+
+        -- OpenCL 	
+    -- 'opencl_ls',
+
+        -- PHP 	
+    -- 'intelephense',
+
+        -- PHP 	
+    -- 'phpactor',
+
+        -- PHP 	
+    -- 'psalm',
+
+        -- Perl 	
+    -- 'perlnavigator',
+
+        -- Powershell 	
+    -- 'powershell_es',
+
+        -- Prisma 	
+    -- 'prismals',
+
+        -- Puppet 	
+    -- 'puppet',
+
+        -- PureScript 	
+    -- 'purescriptls',
+
+        -- Python 	
+    -- 'jedi_language_server',
+
+        -- Python 	
+    -- 'pyright',
+
+        -- Python 	
+    -- 'sourcery',
+
+        -- Python 
+    -- 'pylsp',
+
+        -- R 	
+    -- 'r_language_server',
+
+        -- ReScript 	
+    -- 'rescriptls',
+
+        -- Reason 	
+    -- 'reason_ls',
+
+        -- Robot Framework 	
+    -- 'robotframework_ls',
+
+        -- Rome 	
+    -- 'rome',
+
+        -- Ruby 	
+    -- 'solargraph',
+
+        -- Rust 	
+    -- 'rust_analyzer',
+
+        -- SQL 	
+    -- 'sqlls',
+
+        -- SQL 	
+    -- 'sqls',
+
+        -- Salt 	
+    -- 'salt_ls',
+
+        -- Shopify Check  
+    -- 'theme_check',
+
+        -- Slint 	
+    -- 'slint_lsp',
+
+        -- Solidity 	
+    -- 'solang',
+
+        -- Solidity 	
+    -- 'solc',
+
+        -- Solidity 
+    -- 'solidity_ls',
+
+        -- Sorbet 	
+    -- 'sorbet',
+
+        -- Sphinx 	
+    -- 'esbonio',
+
+        -- Stylelint 	
+    -- 'stylelint_lsp',
+
+        -- Svelte 	
+    -- 'svelte',
+
+        -- Swift 	
+    -- 'sourcekit',
+
+        -- SystemVerilog 	
+    -- 'svls',
+
+        -- SystemVerilog 	
+    -- 'verible',
+
+        -- TOML 	
+    -- 'taplo',
+
+        -- Tailwind CSS 	
+    -- 'tailwindcss',
+
+        -- Teal 	
+    -- 'teal_ls',
+
+        -- Terraform 	
+    -- 'terraformls',
+
+        -- Terraform 
+    -- 'tflint',
+
+        -- TypeScript 	
+    -- 'ttsserver',
+
+        -- V 	
+    -- 'vls',
+
+        -- Vala 	
+    -- 'vala_ls',
+
+        -- VimL 	
+    -- 'vimls',
+
+        -- Vue 	
+    -- 'volar',
+
+        -- Vue 	
+    -- 'vuels',
+
+        -- XML 	
+    -- 'lemminx',
+
+        -- YAML 	
+    -- 'yamlls',
+
+        -- Zig 	
+    -- 'zls',
+}
+
+return servers
